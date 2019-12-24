85eaeae4110d6d84509e45245e2648ba51de23f0
Anderson <34724755+andersonaddo@users.noreply.github.com>
2019-12-23 17:04:19 -0800

    Added icons to the tab nav

--- #screens/MainTabNav.js
+++ #screens/MainTabNav.js
@@ -3,6 +3,7 @@
 import React from 'react'
 import auth from '@react-native-firebase/auth';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import AwesomeIcon from 'react-native-vector-icons/FontAwesome5';
 
 import Feed from './FeedSection/Feed'
 import LogOut from "./Settings/LogOut"
@@ -10,32 +11,58 @@ import Active from "./DashboardSection/ActiveBroadcasts"
 import FriendSwitchNav from './FriendSection/FriendSectionStackNav'
 
 
-const Tab = createBottomTabNavigator({
-  Active,
-  Feed,
-  FriendSwitchNav,
-  LogOut,
-});
+const Tab = createBottomTabNavigator(
+  {
+    Active,
+    Feed,
+    FriendSwitchNav,
+    LogOut,
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) =>
+      ({
+        tabBarIcon: ({ focused, horizontal, tintColor }) => {
+          const { routeName } = navigation.state;
+          let iconName;
+          if (routeName === 'Active') {
+            iconName = `home`;
+          } else if (routeName === 'Feed') {
+            iconName = `rss`;
+          }else if (routeName === 'FriendSwitchNav') {
+            iconName = `user-friends`;
+          }else{
+            iconName = `cog`;
+          }
+
+          return <AwesomeIcon name={iconName} size={25} color={tintColor} />;
+        },
+      }),
+    tabBarOptions: {
+      activeTintColor: 'tomato',
+      inactiveTintColor: 'gray',
+    },
+  }
+);
 
 export default class Main extends React.Component {
 
-    //https://reactnavigation.org/docs/en/common-mistakes.html
-    static router = Tab.router;
+  //https://reactnavigation.org/docs/en/common-mistakes.html
+  static router = Tab.router;
 
-    constructor(props){
-      super(props)
-      global.MainTabRoot = this;
-    }
+  constructor(props) {
+    super(props)
+    global.MainTabRoot = this;
+  }
 
-    render() {
-      return (
-          <Tab navigation={this.props.navigation}/>
-      )
-    }
+  render() {
+    return (
+      <Tab navigation={this.props.navigation} />
+    )
+  }
 
-    signOut = () => {
-      auth().signOut()
+  signOut = () => {
+    auth().signOut()
       .then(() => this.props.navigation.navigate("AuthDecisionLander"))
       .catch(() => console.error("Something went wrong with signing out!"))
-    }
   }
+}
