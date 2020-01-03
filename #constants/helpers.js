a1700352f4050e30f2f88110bfe4c84cb2503f7c
Anderson <34724755+andersonaddo@users.noreply.github.com>
2020-01-03 12:33:56 -0800

    Increased Timeouts

--- #constants/helpers.js
+++ #constants/helpers.js
@@ -24,9 +24,9 @@ export const timedPromise = (promise, ms) => {
 }
 
 
-export const SHORT_TIMEOUT = 3000
-export const MEDIUM_TIMEOUT = 5000
-export const LONG_TIMEOUT = 10000
+export const SHORT_TIMEOUT = 7000
+export const MEDIUM_TIMEOUT = 10000
+export const LONG_TIMEOUT = 15000
 
 
 /**
