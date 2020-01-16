906156f708b93bae143b5d53792628a42015b6c8
Anderson <34724755+andersonaddo@users.noreply.github.com>
2020-01-15 20:44:03 -0500

    Added QR Code for adding friends

--- #screens/Settings/LogOut.js
+++ #screens/Settings/LogOut.js
@@ -2,26 +2,30 @@
 
 import React from 'react'
 import { StyleSheet, Platform, Image, Text, View, Button } from 'react-native'
+import QRCode from 'react-native-qrcode-svg';
 import auth from '@react-native-firebase/auth';
-import database from '@react-native-firebase/database';
 
 import ProfilePicChanger from '../../#reusableComponents/ProfilePicChanger'
-import {timedPromise, MEDIUM_TIMEOUT} from '../../#constants/helpers' 
-
+import ProfilePicDisplayer from '../../#reusableComponents/ProfilePicDisplayer';
 
 export default class LogOut extends React.Component {
 
-  state = {currentUser: null, picPath: ""}
-
-  componentDidMount() {
-    const { currentUser } = auth()
-    this.setState({ currentUser })
-  }
-
     render() {
-      const { currentUser } = this.state
+      const { currentUser } = auth()
       return (
         <View style={styles.container}>
+          <View style = {styles.QRHolder}>
+            <QRCode
+              value = {currentUser.uid}
+              logoBackgroundColor = 'transparent'
+              backgroundColor = "white"
+              color = "orange"
+              size = {180}
+              logo = {require('../../_media/WhiteCircle.png')}
+              logoSize = {80}
+              ecl = "H"/>
+            <ProfilePicDisplayer diameter = {70} uid = {currentUser.uid} style = {styles.profilePic} />
+          </View>
           <Text>
             Hi {currentUser && currentUser.email}!
           </Text>
@@ -40,5 +44,19 @@ export default class LogOut extends React.Component {
       flex: 1,
       justifyContent: 'center',
       alignItems: 'center'
+    },
+    QRHolder:{
+      borderRadius: 20, 
+      padding: 10, 
+      justifyContent: "center", 
+      alignContent: "center", 
+      borderColor: "orange", 
+      borderWidth: 8,
+      backgroundColor: "white"
+    },
+    profilePic:{
+      position: "absolute",
+      top: 65, // = QRHolder + (QRCOde size prop / 2) - radius of profile pic displayer
+      left: 65,
     }
   })
