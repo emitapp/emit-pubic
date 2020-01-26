c67196676280b96240850c88def2063493336cc3
Anderson <34724755+andersonaddo@users.noreply.github.com>
2020-01-26 18:40:30 -0500

    Moved all notification creation logic into a separate file

---
+++ #constants/fcmNotificationHandlers.js
@@ -0,0 +1,26 @@
+//This file has the methods that decide what to do upon receiving a FCM message
+import {Notifications} from 'react-native-notifications';
+import auth from '@react-native-firebase/auth';
+import {checkNotifications, RESULTS} from 'react-native-permissions';
+
+export const handleFCMMessage = async (remoteMessage) => {
+    if (!auth().currentUser) return
+    const response = await checkNotifications();
+    if (response.status != RESULTS.GRANTED) return;
+
+    Notifications.postLocalNotification({
+        title: remoteMessage.data.title,
+        body: remoteMessage.data.body ? remoteMessage.data.body : "",
+    });
+}
+
+
+export const handleFCMDeletion = async () => {
+    if (!auth().currentUser) return
+    const response = await checkNotifications();
+    if (response.status != RESULTS.GRANTED) return;
+    Notifications.postLocalNotification({
+        title: "Nice to see you again!",
+        body: "It looks a lot might have happened since you last signed in!",
+      });
+}
