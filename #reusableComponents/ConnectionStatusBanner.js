ea52513d88450ae83a5269ef30566a9ddc25b504
Anderson <34724755+andersonaddo@users.noreply.github.com>
2020-01-29 12:55:14 -0500

    Installed net-info plugin and integrated it with the connection banner

--- #reusableComponents/ConnectionStatusBanner.js
+++ #reusableComponents/ConnectionStatusBanner.js
@@ -3,11 +3,19 @@ import { View, Text, Dimensions, StyleSheet } from 'react-native';
 const { width } = Dimensions.get('window');
 import database from '@react-native-firebase/database'
 import {logError} from '../#constants/helpers'
+import NetInfo from "@react-native-community/netinfo";
 
 export default class OfflineNotice extends PureComponent {
-  state = {
-    isConnected: true
-  };
+
+  constructor (props){
+    super(props)
+    this.state = {
+      connectedToFirebase: false,
+      connectedToNetwork: false,
+      isVisible: false,
+    };
+    this.unsubscribe = null;
+  }
 
   componentDidMount() {
     //For this banner to work, there has to be a constant connection to the database
@@ -15,32 +23,55 @@ export default class OfflineNotice extends PureComponent {
     //https://stackoverflow.com/questions/53320480
     //This creates a constant connection to a dummy location in my database
     database().ref('dummyDatabaseLocation').keepSynced(true).catch(err => logError(err)) 
-    database().ref(".info/connected").on("value", this.hanldeConnectionChange)
-  }
-
-  hanldeConnectionChange = (snap) => {
+    database().ref(".info/connected").on("value", snap => {
       if (!snap) return;
-      this.setState({isConnected: snap.val()})
+      this.setState({connectedToFirebase: snap.val()}, this.decideVisibility)
+    })
+
+    this.unsubscribe = NetInfo.addEventListener(state => {
+      //If https://github.com/react-native-community/react-native-netinfo/issues/254
+      //if ever fixed, might switch to state.isInternetReachable
+      this.setState({connectedToNetwork: state.isConnected}, this.decideVisibility);
+    });
   }
 
   componentWillUnmount(){
+    //cannot call keepSynced(true) because it's asynchronous
     database().ref(".info/connected").off()
+    this.unsubscribe()
+  }
+
+  decideVisibility = () => {
+    let shouldBeVisible = !this.state.connectedToFirebase
+    if (shouldBeVisible) this.setState({isVisible: true})
+    else setTimeout(() => this.setState({isVisible: false}), 2000);
   }
 
   render() {
-    if (!this.state.isConnected) {
+    if (!this.state.isVisible) return null;
+    if (this.state.connectedToFirebase) {
       return (
-      <View style={styles.offlineContainer}>
-        <Text style={styles.offlineText}>Not connected to Biteup Cloud</Text>
+      <View style={[styles.networkBanner, {'backgroundColor': "green"}]}>
+        <Text style={styles.offlineText}>Connected to Biteup servers!</Text>
       </View>);
     }
-    return null;
+
+    if (this.state.connectedToNetwork) {
+      return (
+        <View style={[styles.networkBanner, {'backgroundColor': "gold"}]}>
+        <Text style={styles.offlineText}>Attempting to reach Biteup servers...</Text>
+      </View>);
+    }
+
+    return (
+      <View style={[styles.networkBanner, {'backgroundColor': "red"}]}>
+      <Text style={styles.offlineText}>No network access</Text>
+    </View>);
   }
 }
 
 const styles = StyleSheet.create({
-  offlineContainer: {
-    backgroundColor: '#b52424',
+  networkBanner: {
     height: 30,
     justifyContent: 'center',
     alignItems: 'center',
