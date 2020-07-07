e7b1f6f41ba204ce224c8b9b324b69cec223f606
Anderson <34724755+andersonaddo@users.noreply.github.com>
2020-07-07 12:29:36 -0400

    Fixed syntax error in improved FCM background message handling

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
+import React from 'react'
 import {name as appName} from './app.json';
 import 'react-native-gesture-handler' //For react-navigation
 import messaging from '@react-native-firebase/messaging';
 import { handleFCMMessage } from 'utils/fcmNotificationHandlers';
 import codePush from "react-native-code-push";
+
+const CodePushApp = codePush(App)
 
 messaging().setBackgroundMessageHandler(async (remoteMessage) => {
     await handleFCMMessage(remoteMessage)
@@ -14,8 +17,9 @@ messaging().setBackgroundMessageHandler(async (remoteMessage) => {
 function HeadlessCheckedApp({ isHeadless }) {
     // If app has been "launched" in the background by iOS due to FCM, ignore
     if (isHeadless) return null;
-    return codePush(App);
+    return <CodePushApp />;
 }
 
-AppRegistry.registerComponent(appName, HeadlessCheckedApp);
+
+AppRegistry.registerComponent(appName, () => HeadlessCheckedApp);
 
